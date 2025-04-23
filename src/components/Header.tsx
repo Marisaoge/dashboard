f22915ede3576aedcf1b5958afe76684805a7b86
Marisa Oge <marisaoge@Marisas-MBP.home>
2025-04-23 10:58:22 -0700

    Make header sticky when scrolling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -155,7 +155,7 @@ const Header: React.FC<HeaderProps> = () => {
   );
 
   return (
-    <header className="bg-white border-b border-gray-200 px-4 lg:px-6 py-4">
+    <header className="bg-white border-b border-gray-200 px-4 lg:px-6 py-4 sticky top-0 z-50">
       <div className="flex items-center justify-between">
         <div className="flex-1 flex items-center">
           {getPageIcon(location.pathname)}
